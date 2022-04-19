5f07d934918a2461048a433330b0b431c4652374
sunny-aguilar <sunny.aguilar@gmail.com>
2022-04-18 20:20:27 -0700

    nak-app

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,6 @@ import 'screens/national_board_screen.dart';
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     const String appTitle = "NAK";
